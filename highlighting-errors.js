e828172066b42382e9f710d69bd240af603343e5
Phillip Lemons <phillip@khanacademy.org>
2015-06-10 16:22:46 -0700

    Added new task for javascript highlighting and indentation

---
+++ highlighting-errors.js
@@ -0,0 +1,8 @@
+// Also does improper indentation
+function twoLines(param1, param2,
+                 param3, param4) {
+                     console.log("FAIL");
+                 }
+
+// Weird regex that is valid
+var regex = /.*coolRegex[^/]/;
