d996a79e9afeff0f55ce12838a7a99828e0bd8a1
Phillip Lemons <philliplemons512@gmail.com>
2019-05-01 20:00:16 -0500

    Remove old highlighting errors files

--- highlighting-errors.js
+++
@@ -1,8 +0,0 @@
-// Also does improper indentation
-function twoLines(param1, param2,
-                 param3, param4) {
-                     console.log("FAIL");
-                 }
-
-// Weird regex that is valid
-var regex = /.*coolRegex[^/]/;
--- highlighting-errors.py
+++
@@ -1,7 +0,0 @@
-@params(keys=['usrname', 'params'], other_key="hello")
-def my_func(keys=['hello', 'goodbye']):
-    print keys
-    return "Hello"
-
-def func(a, b, c):
-    return
